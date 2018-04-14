3e4aa656ab77283f5fd99110306d0941e7b302c7
bvasilop <bvasilop@gmail.com>
2018-04-14 01:40:53 -0700

    reverted to dist settings for serviceworker caching

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,7 +106,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'vue-news2',
       filename: 'service-worker.js',
-      staticFileGlobs: ['build/**/*.{js,html,css}'],
+      staticFileGlobs: ['docs/**/*.{js,html,css}'],
       minify: true,
       stripPrefix: 'dist/'
     })
