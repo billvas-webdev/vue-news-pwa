a6451c12daef14f9719ff8f1b6aeafb1a5e5a970
bvasilop <bvasilop@gmail.com>
2018-04-13 19:14:56 -0700

    changed SWPrecacheWebpackPlugin params from dist/ to build/ in webpack.prod.conf.js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,9 +106,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'vue-news2',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['build/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'build/'
     })
   ]
 })
