5b48bd217ab1476dbce01428675c7b1397420f36
bvasilop <bvasilop@gmail.com>
2018-04-20 22:01:17 -0700

    changed weather summary src to https to avoid mixed use errors

--- src/components/WeatherSummary.vue
+++ src/components/WeatherSummary.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="weatherSummary in weatherData" class="weatherSummary">
-            <img v-bind:src="'http://openweathermap.org/img/w/' + weatherSummary.icon + '.png'" v-bind:alt="weatherSummary.main">
+            <img v-bind:src="'https://openweathermap.org/img/w/' + weatherSummary.icon + '.png'" v-bind:alt="weatherSummary.main">
             <br>
             <b>{{ weatherSummary.main }}</b>
         </div>
