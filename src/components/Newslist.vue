2f93fb59746afae740ca72187f5033b48cb82790
bvasilop <bvasilop@gmail.com>
2020-06-08 23:14:28 -0700

    removed proxy prefix

--- src/components/Newslist.vue
+++ src/components/Newslist.vue
@@ -78,7 +78,7 @@ export default {
     updateSource(source) {
       this.axios
         .get(
-          "https://desolate-retreat-58651.herokuapp.com/https://newsapi.org/v2/top-headlines?sources=" +
+          "https://newsapi.org/v2/top-headlines?sources=" +
             source +
             "&apiKey=30fdd9c8493742eebe75a786fc36f1bd"
         )
