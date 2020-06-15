289cbfb2a9be2a552bc7799ca7ccdf41e7538bb0
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2020-06-14 23:13:07 -0700

    added cors url

--- src/components/Newslist.vue
+++ src/components/Newslist.vue
@@ -78,7 +78,7 @@ export default {
     updateSource(source) {
       this.axios
         .get(
-          "https://newsapi.org/v2/top-headlines?sources=" +
+          "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?sources=" +
             source +
             "&apiKey=30fdd9c8493742eebe75a786fc36f1bd"
         )
